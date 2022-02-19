99d8448f67b81a93865a69f78d8807c872d334cc
Santiago <santiagofgaray@gmail.com>
2022-02-19 23:04:20 +0100

    lab finished

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 const mongoose = require('mongoose');
+require('dotenv').config(); //require('dotenv/config')
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = `mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.6fts6.mongodb.net/recipe-app`;
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    Recipe.deleteMany() // Before adding any recipes to the database, let's remove all existing ones
+    .then(() => {
+      Recipe.insertMany(data)
+      /*.then((recipe) => {
+      data.forEach((recipe) => {console.log(recipe.title);})
+      })*/
+      .then(() => {
+        Recipe.findOneAndUpdate(
+          {title: "Rigatoni alla Genovese"}, 
+          {$set: {duration: 100}},
+          { new: true }
+        )
+        .then((response) => {console.log(response)})
+        .then(() => {
+          Recipe.deleteOne({title:"Carrot Cake"})
+          .then((response) => {console.log(response)})
+        })
+      })
+    })
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+   
+
+
+
+
+
+// Recipe.create({
+//         title: "Asado",
+//         level: "Easy Peasy",
+//         ingredients: [
+//           "vacio",
+//           "molleja",
+//           "entraña",
+//           "costillar",
+//           "matambrito",
+//           "provoleta",
+//           "chorizo",
+//           "morcilla"  
+//         ],
+//         cuisine: "Argentinian",
+//         dishType: "main_course",
+//         image:
+//           "https://rinconrecetas.com/wp-content/uploads/2014/10/parrilla.jpg",
+//         duration: 120,
+//         creator: "Gaucho",
+//       });
